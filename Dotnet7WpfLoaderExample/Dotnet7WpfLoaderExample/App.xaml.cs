f306867766fd14e9885a4f0643606e3172aa5cfc
OmatrixTech <144843226+OmatrixTech@users.noreply.github.com>
2023-11-22 00:26:41 +0530

    All Files added

---
+++ Dotnet7WpfLoaderExample/Dotnet7WpfLoaderExample/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DotNet7.Wpf.CustomLoader;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Dotnet7WpfLoaderExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider? serviceProvider = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Create a service collection
+            var services = new ServiceCollection();
+
+            // Register your services
+            services.AddSingleton<ICustomLoaderService, CustomLoaderService>();
+            services.AddTransient<MainWindowViewModel>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Dotnet7WpfLoaderExample/Dotnet7WpfLoaderExample/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DotNet7.Wpf.CustomLoader.Utility;
+using DotNet7.Wpf.CustomLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Dotnet7WpfLoaderExample
+{
+    public class MainWindowViewModel
+    {
+        ICustomLoaderService? customLoaderService = null;
+        DispatcherTimer? timer = null;
+        public ICommand ShowLoaderWithoutInstance { get; }
+        public MainWindowViewModel(ICustomLoaderService customLoaderService)
+        {
+            this.customLoaderService = customLoaderService;
+            ShowLoaderWithoutInstance = new RelayCommand(()=>ShowLoader());
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(10); // Adjust the interval as needed
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            //Dependency injection service handler
+           // customLoaderService.ShowLoader();
+
+            //Without instance method
+            LoaderHandler.ShowLoader();
+        }
+
+        private void ShowLoader()
+        {
+            LoaderHandler.ShowLoader();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Dependency injection service handler
+           // customLoaderService.HideLoader();
+
+            //Without instance method
+            LoaderHandler.HideLoader();
+        }
+    }
+}
